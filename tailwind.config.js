383ebc4ab5fad4badffbe269c51929ca7960d096
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2022-06-09 12:06:44 +0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	content: ['./dist/**/*.html'],
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+		},
+		colors: {
+			accent: '#765447',
+			secondBlue: '#00AAD2',
+			sand: '#E4DCD3',
+			transparent: 'transparent',
+			current: 'currentColor',
+			blue: colors.blue,
+			black: colors.black,
+			white: colors.white,
+			gray: colors.neutral,
+			indigo: colors.indigo,
+			red: colors.rose,
+			green: colors.emerald,
+			yellow: colors.amber,
+			purple: colors.violet,
+		},
+		extend: {
+			fontFamily: {
+				sans: ['MS', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+}
