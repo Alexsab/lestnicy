83aca642e0d3e3bad2c2f0e571837d483acc4ba5
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2022-06-16 11:44:43 +0500

    scrollIntoView

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,9 +1,7 @@
 const menu = document.querySelector('.top-line');
 const menuBtn = document.getElementById('nav-icon2');
 menuBtn.addEventListener('click', () => {
-	menuBtn.classList.toggle('open')
-	menu.classList.toggle('hidden')
-	document.body.classList.toggle('no-scroll')
+	toggleClasses()
 })
 const setMenuHeight = () => {
 	if(window.innerWidth < 768){
@@ -15,3 +13,34 @@ const setMenuHeight = () => {
 }
 setMenuHeight();
 window.addEventListener('resize', setMenuHeight);
+
+const toggleClasses = () => {
+	menuBtn.classList.toggle('open')
+	menu.classList.toggle('hidden')
+	document.body.classList.toggle('no-scroll')
+}
+
+const scroll = () => {
+	const anchors = document.querySelectorAll('a[href^="#"]');
+	for (let anchor of anchors) {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+			const blockID = anchor.getAttribute('href').substr(1);
+			if (blockID) {
+				const parent = anchor.closest('.top-line');
+				if(parent){
+					toggleClasses()
+				}
+				// anchors.forEach(item => {
+				// 	item.classList.remove('bg-accent', 'text-white');
+				// })
+				// anchor.classList.add('bg-accent', 'text-white');
+				document.getElementById(blockID).scrollIntoView({
+					behavior: 'smooth',
+					block: 'start'
+				})
+			}
+		})
+	}
+}
+scroll()
