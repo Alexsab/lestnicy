1870fdacfb5265c7ecb3f52c8d92d6f4d18594c9
drsbnv <90459119+drsbnv@users.noreply.github.com>
2022-11-07 22:30:51 +0300

    Up 2

--- src/js/modules/galary.js
+++ src/js/modules/galary.js
@@ -18,13 +18,5 @@ window.addEventListener('resize', setImagesHeight);
 require('fslightbox');
 
 const galary = [
-	{url: 'img/about/1.png', thumb: 'img/about/1.png'},
-	{url: 'img/about/2.jpg', thumb: 'img/about/2.jpg'},
-	{url: 'img/about/3.jpg', thumb: 'img/about/3.jpg'},
-	{url: 'img/about/1.png', thumb: 'img/about/1.png'},
-	{url: 'img/about/2.jpg', thumb: 'img/about/2.jpg'},
-	{url: 'img/about/3.jpg', thumb: 'img/about/3.jpg'},
-	{url: 'img/about/1.png', thumb: 'img/about/1.png'},
-	{url: 'img/about/2.jpg', thumb: 'img/about/2.jpg'},
 ]
 export default galary
