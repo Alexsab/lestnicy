188b42090ac4948bda5ae19cb8002f6fbe81e0a1
Alexander Sabanov <alexsab@mail.ru>
2022-11-15 15:54:14 +0400

    upd forms and scripts

--- src/js/modules/galary.js
+++ src/js/modules/galary.js
@@ -19,13 +19,13 @@ require('fslightbox');
 
 const galary = [
 
-{url: 'img/about/10.jpg', thumb: 'img/about/10.jpg'},
+	{url: 'img/about/10.jpg', thumb: 'img/about/10.jpg'},
 	{url: 'img/about/44.jpg', thumb: 'img/about/44.jpg'},
 	{url: 'img/about/5.jpg', thumb: 'img/about/5.jpg'},
-{url: 'img/about/45.jpg', thumb: 'img/about/45.jpg'},
+	{url: 'img/about/45.jpg', thumb: 'img/about/45.jpg'},
 	{url: 'img/about/6.jpg', thumb: 'img/about/6.jpg'},
 	{url: 'img/about/7.jpg', thumb: 'img/about/7.jpg'},
-{url: 'img/about/76.jpg', thumb: 'img/about/76.jpg'},
+	{url: 'img/about/76.jpg', thumb: 'img/about/76.jpg'},
 	{url: 'img/about/9.jpg', thumb: 'img/about/9.jpg'},
 ]
 export default galary
